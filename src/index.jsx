bee909553eea0df73456cdb2ea1f210a2160fe08
Alex Todoran <todoran_alex@yahoo.com>
2023-02-21 21:09:34 +0200

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import Application from "./Application";
+
+import "./tailwind.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Application />
+  </React.StrictMode>
+);
